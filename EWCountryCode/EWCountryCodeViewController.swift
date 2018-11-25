422147d4a642fb4d7886c78c738315854908ac06
Ethan.W <wang197200@163.com>
2018-11-25 19:56:16 +0800

    Update EWCountryCodeViewController.swift

--- EWCountryCode/EWCountryCodeViewController.swift
+++ EWCountryCode/EWCountryCodeViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+/// 获取系统语言方法
 fileprivate func getCurrentLanguage() -> String {
     let preferredLang = Bundle.main.preferredLocalizations.first! as NSString
     switch String(describing: preferredLang) {
@@ -20,16 +20,15 @@ fileprivate func getCurrentLanguage() -> String {
     }
 }
 class EWCountryCodeViewController: UIViewController {
-
+    /// 选中国家后的闭包回调
     public var backCountryCode: ((String, String)->())?
     private var tableView: UITableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
     private var searchController: UISearchController?
     private var sortedNameDict: Dictionary<String, Any>?
     private var indexArray: Array<String>?
+    /// 筛选出后的结果array
     private var results: Array<Any> = Array()
-    var ob: NSKeyValueObservation!
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = isLanguageEnglish() ? "Country/Location" : "选择国家/地区"
@@ -49,18 +48,18 @@ class EWCountryCodeViewController: UIViewController {
         searchController?.searchResultsUpdater = self
         searchController?.dimsBackgroundDuringPresentation = false
         searchController?.hidesNavigationBarDuringPresentation = false
-        searchController?.searchBar.placeholder = "Search"
+        searchController?.searchBar.placeholder = isLanguageEnglish() ? "Search" : "搜索"
         let cancel: String = isLanguageEnglish() ? "Cancel" : "取消"
         searchController?.searchBar.setValue(cancel, forKey:"_cancelButtonText")
         tableView.tableHeaderView = searchController?.searchBar
+        /// 从plist文件中获取数据并存储
         let sortedName = isLanguageEnglish() ? "sortedNameEN" : "sortedNameCH"
         let path = Bundle.main.path(forResource: sortedName, ofType: "plist")
         sortedNameDict = NSDictionary(contentsOfFile: path ?? "") as? Dictionary<String, Any>
         indexArray = Array(sortedNameDict!.keys).sorted(by: {$0 < $1})
     }
-
-    
-    func showCodeStringIndex(indexPath: NSIndexPath) -> String{
+    /// 从存储数据中获取每条的国家+区号String
+    private func showCodeStringIndex(indexPath: NSIndexPath) -> String{
         var showCodeString: String = ""
         if searchController!.isActive{
             if results.count > indexPath.row{
@@ -76,7 +75,7 @@ class EWCountryCodeViewController: UIViewController {
         }
         return showCodeString
     }
-    func selectCodeIndex(indexPath: IndexPath){
+    private func selectCodeIndex(indexPath: IndexPath){
         let originText = self.showCodeStringIndex(indexPath: indexPath as NSIndexPath)
         let array = originText.components(separatedBy: "+")
         let countryName = array.first?.trimmingCharacters(in: CharacterSet.whitespaces)
@@ -134,6 +133,7 @@ extension EWCountryCodeViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return searchController!.isActive ? 0 : 30
     }
+    /// 右侧的筛选sectionTitle
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if ((indexArray?.count) != nil) && indexArray!.count > section {
             return indexArray?[section]
@@ -145,6 +145,7 @@ extension EWCountryCodeViewController:UITableViewDelegate,UITableViewDataSource{
     }
 }
 extension EWCountryCodeViewController: UISearchResultsUpdating{
+    /// searchResults代理方法，将搜索到的内容加入resultArray 赋给tableView
     func updateSearchResults(for searchController: UISearchController) {
         if results.count > 0 {
             results.removeAll()
